ba5fea61fad2c4f0949cc27f4def6fe8733c213f
ShatteredTrace <lk03@arcor.de>
2018-06-07 14:34:36 +0200

    Stuff

---
+++ Assets/Code/Hero.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero{
+
+    int Hitpoints;
+    int Manapoints;
+    int Maxhandsize;
+
+    Hero(int hp, int mp, int maxhand)
+    {
+        this.Hitpoints = hp;
+        this.Manapoints =mp;
+        this.Maxhandsize = maxhand;
+    }
+
+    Hero()
+    {
+        this.Hitpoints = 10;
+        this.Manapoints = 10;
+        this.Maxhandsize = 10;
+    }
+
+    void takeDamage(int dmg)
+    {
+        Hitpoints -= dmg;
+    }
+
+    bool useMana(int mana)
+    {
+        if(Manapoints >= mana)
+        {
+            Manapoints -= mana;
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Code/Monster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster
+{
+    int HitPoints;
+    int Damage;
+    int Speed;
+
+    public Monster(int hp, int dmg, int speed)
+    {
+        this.HitPoints = hp;
+        this.Damage = dmg;
+        this.Speed = speed;
+    }
+
+    public Monster()
+    {
+        this.HitPoints = 100;
+        this.Damage = 1;
+        this.Speed = 1;
+    }
+
+    public void takeDamage(int dmg)
+    {
+        this.HitPoints -= dmg;
+    }
+}
